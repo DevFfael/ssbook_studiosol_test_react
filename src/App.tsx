623d5e75d5d15c3815069b3fee9683a2869a12f4
Raffael Oliveira <raffaeloliveirap@gmail.com>
2023-11-02 18:17:05 -0300

    feat: add logo and header on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import BottomNavigation from './components/BottomNaviagation';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Home />} />
       </Routes>
+      <BottomNavigation />
     </BrowserRouter>
   );
 }
