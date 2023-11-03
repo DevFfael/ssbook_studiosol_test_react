1d0767132f2398954d726b367b0f438203b72c0d
Raffael Oliveira <raffaeloliveirap@gmail.com>
2023-11-03 15:50:30 -0300

    fix: fix footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import BottomNavigation from './components/BottomNaviagation';
+import BookReview from './pages/BookReview';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Home />} />
+        <Route path="/book/:id" element={<BookReview />} />
       </Routes>
       <BottomNavigation />
+      <Footer />
     </BrowserRouter>
   );
 }
